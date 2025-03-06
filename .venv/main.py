90bcc31fa106391b43544729601ee31264cb63a2
mateiporojan <porojanmatei@gmail.com>
2025-03-06 22:43:33 +0100

    comit 2

---
+++ .venv/main.py
@@ -0,0 +1,11 @@
+print(7+11)
+print(7-11)
+print(7*11)
+print(type(9/3), 9/3)
+print(None) #special value of Null
+print(2**10) #"**" to the power of
+print(8//2) #integer division
+print(9%4) #remainder??
+print(8%9)
+print(44%9)
+print(1 and 2 and false and 9)
